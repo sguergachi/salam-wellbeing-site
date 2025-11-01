e9cc061469860aae393906bc55ea8924a01039e6
Sammy Guergachi <sguergachi@gmail.com>
2025-11-01 11:57:27 -0400

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  server: {
+    port: 4321,
+    host: true,
+  },
+  vite: {
+    logLevel: "error",
+  },
 });
